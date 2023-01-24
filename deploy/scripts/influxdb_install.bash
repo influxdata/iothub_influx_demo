de27e36ff0e1a1b2dcf89fa866ea3ac059761b7f
Josh Powers <powersj@fastmail.com>
2023-01-24 15:36:17 -0700

    chore: update package repo GPG key

--- deploy/scripts/influxdb_install.bash
+++ deploy/scripts/influxdb_install.bash
@@ -2,9 +2,9 @@
 
 # Installing InfluxDB
 
-wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-source /etc/lsb-release
-echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+wget -q https://repos.influxdata.com/influxdata-oss-archive_compat.key
+echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-oss-archive_compat.key' | sha256sum -c && cat influxdata-oss-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-oss-archive_compat.gpg > /dev/null
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-oss-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
 sudo apt-get update && sudo apt-get install -y influxdb
 sudo systemctl unmask influxdb.service
 sudo systemctl start influxdb
@@ -14,16 +14,16 @@ curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE "o
 
 # Installing Kapacitor
 
-# Kapacitor provides most of the functionality of Chronograf. 
-# In particular it is responsible for sending alerts. 
-# Alerts can be sent using various different services ranging from SMTP to Slack and HipChat. 
+# Kapacitor provides most of the functionality of Chronograf.
+# In particular it is responsible for sending alerts.
+# Alerts can be sent using various different services ranging from SMTP to Slack and HipChat.
 wget https://dl.influxdata.com/kapacitor/releases/kapacitor_1.5.3_amd64.deb
 sudo dpkg -i kapacitor_1.5.3_amd64.deb
 sudo systemctl enable kapacitor
 sudo systemctl start kapacitor
 
 #Telegraf
-# After installing InfluxDB and Kapacitor successfully, we can continue with installing Telegraf. 
+# After installing InfluxDB and Kapacitor successfully, we can continue with installing Telegraf.
 # Telegraf is responsible for gathering all metrics which will further be visualized through Chronograf.
 
 wget https://dl.influxdata.com/telegraf/releases/telegraf_1.12.2-1_amd64.deb
