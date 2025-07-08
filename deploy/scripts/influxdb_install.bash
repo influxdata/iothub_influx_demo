abd948c39d09104deee76c266d2db3162290c802
Jamie Strandboge <jamie@influxdata.com>
2025-07-08 14:30:16 -0500

    chore: update influxdb_install.bash to use influxdata-archive.key

--- deploy/scripts/influxdb_install.bash
+++ deploy/scripts/influxdb_install.bash
@@ -2,9 +2,9 @@
 
 # Installing InfluxDB
 
-wget -q https://repos.influxdata.com/influxdata-archive_compat.key
-echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
-echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
+wget -q https://repos.influxdata.com/influxdata-archive.key
+gpg --no-default-keyring --homedir /nonexistent --show-keys ./influxdata-archive.key | grep -q "24C975CBA61A024EE1B631787C3D57159FC2F927" && cat influxdata-archive.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive.gpg > /dev/null
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
 sudo apt-get update && sudo apt-get install -y influxdb
 sudo systemctl unmask influxdb.service
 sudo systemctl start influxdb
